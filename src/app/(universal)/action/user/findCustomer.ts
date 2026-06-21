cfa138b2e5061f2cedb5315f2393a0e8ff464350
Gurjit <gurjiitsingh@gmail.com>
2026-06-21 18:47:27 +0530

    auto address fech on checkout

--- src/app/(universal)/action/user/findCustomer.ts
+++ src/app/(universal)/action/user/findCustomer.ts
@@ -6,22 +6,25 @@ import { adminDb } from "@/lib/firebaseAdmin";
 
 export async function findCustomer(identifier: string) {
   const value = identifier.trim();
-
+  console.log("value----------------", value)
   const usersRef = adminDb.collection("users");
 
   const field = value.includes("@") ? "email" : "phone";
 
   const snap = await usersRef
-    .where(field, "==", value)
-    .limit(1)
-    .get();
-
+  .where("mobNo", "==", value)
+  .limit(1)
+  .get();
   if (snap.empty) {
+    console.log("not find----------------")
     return null;
   }
 
-  return {
-    id: snap.docs[0].id,
-    ...snap.docs[0].data(),
-  };
+  const data = snap.docs[0].data();
+
+return {
+  id: snap.docs[0].id,
+  ...data,
+  createdAt: data.createdAt?.toDate().toISOString() ?? null,
+};
 }
