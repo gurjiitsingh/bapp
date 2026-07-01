68b8ec26374609b934d481c66b2d04be157fdb15
Gurjit <gurjiitsingh@gmail.com>
2026-07-01 16:36:38 +0530

    correct

--- src/app/(universal)/action/inventory/rawInventory/applyRawInventoryWrites.ts
+++ src/app/(universal)/action/inventory/rawInventory/applyRawInventoryWrites.ts
@@ -20,7 +20,7 @@ export async function applyRawInventoryWrites(
       updatedAt: now,
     });
 
-    console.log("u----------------", u)
+     
     // ✅ create ledger
     const ledgerRef = adminDb.collection("stockLedgerInventory").doc();
 
--- src/app/(universal)/admin/inventory/supplier/stock-return/page.tsx
+++ src/app/(universal)/admin/inventory/supplier/stock-return/page.tsx
@@ -37,6 +37,7 @@ export default async function Page() {
       );
     }
   }
+ 
 
   return (
    <SupplierInventoryrReturnForm
