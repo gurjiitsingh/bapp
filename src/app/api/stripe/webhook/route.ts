f465d0cfe99c6ee840fb54ed252a1a7c911e6b4b
Gurjit <gurjiitsingh@gmail.com>
2026-09-26 20:46:43 +0530

    update

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,8 +1,6 @@
- 
 import { NextRequest, NextResponse } from "next/server";
-import { handleStripeWebhook } from "../../../../../custom/lib/payments/stripe/stripeWebhook";
 
- 
+import { handleStripeWebhook } from "../../../../../custom/lib/payments/stripe/stripeWebhook";
 
 export async function POST(req: NextRequest) {
   const body = await req.text();
